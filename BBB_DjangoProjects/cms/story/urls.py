98084f6bdea1c05511ba53e1e07e6ad86539bd52
ffichtenmueller <frank.fichtenmueller@outlook.com>
2017-02-23 11:39:16 +0530

    changed folderstructure

--- BBB_DjangoProjects/cms/story/urls.py
+++ BBB_DjangoProjects/cms/story/urls.py
@@ -1,17 +1,19 @@
-from django.conf.urls import *
 from django.conf.urls import url
 from story.models import Story
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 info_dict = { 'queryset': Story.objects.all(), 'template_object_name': 'story' }
 
-urlpatterns[
-    url(r'^(?P<slug>[-\w]+)/$', 'object_detail', info_dict, name="cms-story"),
-    url(r'^$', 'object_list', info_dict, name="cms-home"),
-]
+urlpatterns= [
+    url(r'^(?P<slug>[-\w]+)/$', DetailView.as_view(), info_dict, name="cms-story"),
+    url(r'^$', ListView.as_view(), info_dict, name="cms-home"),
+    url(r'^category/(?P<slug>[-\w]+)/$', DetailView.as_view(), name='cms-category'),
+    url(r'^search/$', DetailView.as_view(), name='cms-search'),
+    ]
 
-urlpatterns.append(
-    urlpatterns(
-        url(r'')
-    )
-)
+
+
+
+
 
